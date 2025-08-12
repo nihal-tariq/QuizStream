3bc940fa678f2dc92becae3a39c3f585a0d2d4d8
Nihal Tariq <nihaltariq55@gmail.com>
2025-08-12 15:45:10 +0500

    user table added for authentication

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+# main.py
 from fastapi import FastAPI
+from fastapi import Depends
 from app.db import Base, engine
-from app.models import video, mcqs
-from app.routers import video_upload, chatbot_router, quiz
+from app.models import video, mcqs, user
+from app.routers import video_upload, chatbot_router, quiz, video_list, video_approve, delete_mcqs, auth_router, manage_users
+from app.auth import require_role
 
 app = FastAPI()
 
@@ -10,7 +13,19 @@ app = FastAPI()
 def on_startup():
     Base.metadata.create_all(bind=engine)
 
+# Auth routes
 
-app.include_router(video_upload.router)
-app.include_router(chatbot_router.router)
-app.include_router(quiz.router)
+
+app.include_router(auth_router.router, tags=["Auth"])
+app.include_router(manage_users.router)
+
+# Teacher-only routes
+app.include_router(video_upload.router, dependencies=[Depends(require_role(["teacher"]))])
+app.include_router(video_list.router, dependencies=[Depends(require_role(["teacher"]))])
+app.include_router(video_approve.router, dependencies=[Depends(require_role(["teacher"]))])
+app.include_router(quiz.router, dependencies=[Depends(require_role(["teacher"]))])
+app.include_router(delete_mcqs.router, dependencies=[Depends(require_role(["teacher"]))])
+
+
+# Both teacher and student can access
+app.include_router(chatbot_router.router, dependencies=[Depends(require_role(["teacher", "student"]))])
--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,34 @@
-videos = relationship("Video", back_populates="uploader")
+from sqlalchemy import Column, String, Boolean, Enum
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+from sqlalchemy.sql.sqltypes import DateTime
+from app.db import Base
+import uuid
+import enum
+
+
+class UserRole(str, enum.Enum):
+    teacher = "teacher"
+    student = "student"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    is_active = Column(Boolean, default=True)
+
+class SignupRequest(Base):
+    __tablename__ = "signup_requests"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
+    status = Column(String, default="pending")
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
